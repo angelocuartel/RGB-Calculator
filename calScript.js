8fa07b813df2e4d8f7d79d07d3c0abec63ad4476
angelocuartel <68045263+angelocuartel@users.noreply.github.com>
2021-09-11 14:35:07 +0800

    Add removeNumber function

--- calScript.js
+++ calScript.js
@@ -5,6 +5,15 @@ const inpt = document.getElementById("text-box");
 
 btns.forEach(element => {
     element.addEventListener("click",function(e){
+        if(element.innerText != "<")
        inpt.value += element.innerText;
+       else
+       inpt.value = removeNum(inpt.value);
+       
     })
 });
+
+
+function removeNum(number){
+    return number.length > 0 ? number.substr(0,number.length-1) : number ;
+}
